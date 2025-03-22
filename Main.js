5352f8dca2c96585da8f5bacd81218a96589df42
Shishir707 <mirshr8@gmail.com>
2025-03-22 23:05:41 +0600

    Implement button function

--- Main.js
+++ Main.js
@@ -2,6 +2,10 @@ function showAlert(){
     alert("Limited Time Offer! Get 20% Off Today.");
 }
 
-function addToCart(){
-    alert("Item added to cart!.");
+function addToCart1(){
+    alert("Pran Mango was added Successfully!.");
+}
+
+function addToCart2(){
+    alert("Bombay Chanachur was added Successfully!.");
 }
